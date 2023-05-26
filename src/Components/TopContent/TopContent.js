c6c48dade9b2039211c02a2440a428e7074241d9
Rajalakshmi Duraivelan <121893509+Rajalakshmi-Duraivelan@users.noreply.github.com>
2023-05-26 18:53:05 +0530

    Update TopContent.js

--- src/Components/TopContent/TopContent.js
+++ src/Components/TopContent/TopContent.js
@@ -8,7 +8,7 @@ const TopContent = () => {
       <div className='topContent_container'>
         <h1>Rajalakshmi Duraivelan</h1>
         <p>Full Stack Developer</p>
-        <a href='https://docs.google.com/document/d/1slUXy0GwneWwsTLOAtNpwh6ww74uOFB8/edit?usp=share_link&ouid=110220596006604540874&rtpof=true&sd=true'>
+        <a href='https://drive.google.com/file/d/1XLFUhJ3BfX3GDnky-70b62oYksK788kC/view?usp=sharing'>
           <button className='topContent_downloadBtn'>Download CV</button>
         </a>
         <Link to="projects" smooth={true} duration={500}>
